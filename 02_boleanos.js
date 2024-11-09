4e833295bfee36676268a227fb39a429d93554b7
albecabrera <albe.cabrera@yahoo.de>
2024-11-09 20:27:24 +0100

    haciendo ejercicios

--- 02_boleanos.js
+++ 02_boleanos.js
@@ -1,13 +1,23 @@
-/*! not es la negación
-&& es el y
-|| es el o */
-
+/* ! not es la negación */
 console.log(true);
 console.log(!true);
 console.log(!false);
 console.log(3 > 4);
 console.log(4 > 3);
 
+/* && es el y */
 console.log(true&&true);
 console.log(false&&true);
+
+/* || es el o */ 
+console.log(true||true);
+console.log(true||false);
+console.log(false||false);
+console.log(false||false);
+
+/* || (or) solo es falso cuando ambos datos evaluados son falsos. De lo contrario
+arroja true */
+
+console.log(true||false && false); /* true */
+console.log(false && !(false||true)); /* false */
 
---
+++ 03_ejercicio.js
@@ -0,0 +1,13 @@
+/* console.log(true || true);
+console.log(false || true);
+console.log(false && true); */
+
+// Escribe 3 ejemplos de expresiones usando cada una de las operaciones: !, &&, ||.
+// Imprime el resultado de tus expresiones usando console.log
+
+console.log(!false);
+// true
+ console.log( !! false);
+ //  FALSE
+ console.log(false && false);
+ // FALSE
