ab614bb1398ae8badd4b53f02b3c3f995773d62c
albecabrera <albe.cabrera@yahoo.de>
2024-11-10 22:24:26 +0100

    Tu mensaje de commit

--- 07_ejeciciosStrings.js
+++ 07_ejeciciosStrings.js
@@ -3,7 +3,8 @@ let nombre = "Alberto";
 console.log("Oye subscríbte");
 console.log('Oye subscríbte');
 
-let otroNombre = "Leandro";
+let otroNombre = "Leandrocabrera";
 console.log("Hola" + " " + otroNombre);
 console.log("Hola " + otroNombre);
 console.log(nombre.length);
+console.log(otroNombre.length);
