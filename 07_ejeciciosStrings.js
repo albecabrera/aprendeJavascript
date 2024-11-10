2032757440b6f2a4c783b498747fbe75f1ab3657
albecabrera <albe.cabrera@yahoo.de>
2024-11-10 22:20:02 +0100

    commit

---
+++ 07_ejeciciosStrings.js
@@ -0,0 +1,9 @@
+let nombre = "Alberto";
+
+console.log("Oye subscríbte");
+console.log('Oye subscríbte');
+
+let otroNombre = "Leandro";
+console.log("Hola" + " " + otroNombre);
+console.log("Hola " + otroNombre);
+console.log(nombre.length);
