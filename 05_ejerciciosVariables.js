a5c2014682048e9267aaf3fe9b37f60503a41cc7
albecabrera <albe.cabrera@yahoo.de>
2024-11-10 10:04:14 +0100

    variablesEjerciciosHecho

--- 05_ejerciciosVariables.js
+++ 05_ejerciciosVariables.js
@@ -1,3 +1,11 @@
 // 1. Declara una variable llamada "nombre" y asignale tu nombre como string
-
 let nombre = "Alberto";
+ // . Declara una variable llamada "apellido" y asignale tu apellido como string:
+ let apellido = "Cabrera";
+ // 3. Declara una variable llamada "edad"
+ let edad;
+ // 4. Imprime en la consola las variables nombre, apellido y edad. ¿Qué esperas que imprima?
+// RESULTADO => ALBERTO, CABRERA, 47
+// 5. Asígnale a la variable "edad" tu edad como un número:
+
+// 6. Print out the 'fristName', 'lastName', and 'age' variables. What do you...
