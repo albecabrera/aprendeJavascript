bca33a8bad923f8d2c1302a391520776fb7a1045
albecabrera <albe.cabrera@yahoo.de>
2024-11-10 22:01:24 +0100

    ya se hacer strings

--- 06_strings.js
+++ 06_strings.js
@@ -10,3 +10,10 @@ console.log(nombre);
 console.log(nombre[1]); // pone la letra l que está en segunda posición
 
 console.log("Alberto".indexOf("t"));
+console.log("Alberto".indexOf("to"));
+
+//CONCATENAR STRINGS
+console.log("Hola" + " " +"mundo");
+//CONCATENAR STRINGS
+console.log("Hola" + " " +"(nombre)");
+
