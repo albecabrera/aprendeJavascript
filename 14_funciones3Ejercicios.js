79822d5ce931922e9275b4972544510ef52876dd
albecabrera <albe.cabrera@yahoo.de>
2024-11-15 20:47:38 +0100

    pasar un argumento a una función

---
+++ 14_funciones3Ejercicios.js
@@ -0,0 +1,21 @@
+// Crear una función esCinco, que regresará TRUE    si un número es igual a 5, y FALSE si no lo es. 
+function esCinco(numero) {
+    if(numero = 5) {
+      return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(esCinco(5));
+
+// Crear una función esPar que toma un número como argumento, y devuelve TRUE si el número es par, y regreswa FALSE si no lo esCinco. 
+function esPar(numeroArgumento) {
+  if(numeroArgumento % 2 == 0) {
+    return true;
+  }
+  else {
+    return false
+  }
+}
+console.log(numeroArgumento(2));
