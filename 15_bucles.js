2d055c3fe53376e88030b2aecf0ffc83907bb699
albecabrera <albe.cabrera@yahoo.de>
2024-11-15 21:57:39 +0100

    Aprendiendo a hacer un for

--- 15_bucles.js
+++ 15_bucles.js
@@ -1,7 +1,18 @@
 // BUCLE, WHILE => reitera, da vueltas
 
- let contador = 9;
+ let contador = 3;
  while(contador < 10) {
     console.log("Diste un paso");
     contador++;
+ }
+
+ let i = 8;
+ while(i<10) {
+   console.log("Diste otro paso");
+   i++;
+ }
+
+ // FOR 
+ for(let i = 0; i >10; i++) {
+   console.log("Diste otros pasos más");
  }
