ce25679a3e90b87d84f69eb7d44c3ba3c2a66901
albecabrera <albe.cabrera@yahoo.de>
2024-11-17 02:07:29 +0100

    fizzBuzz2

--- 18_fizzBuzz.js
+++ 18_fizzBuzz.js
@@ -1,21 +1,13 @@
-const { nodeModuleNameResolver } = require("typescript");
-
 // Definir una función fizzBuzz(max) que toma un número e imprime cada número del 0 al máximo (no incluido) que sea divisible por 3 o 5, pero no ambos. 
 function fizzBuzz(max) {
-   for(let i =  0; i < max; i++) {
-        if( 1% 3 = 0 = ) {
-            if(!(i % 3 = 0 && i % 5)) {
-
-            }
-             {
-
-            }
-        }
+  for (let i = 0; i < max; i++) {
+    // Verificar si el número es divisible por 3 o por 5, pero no ambos
+    if ((i % 3 === 0 || i % 5 === 0) && !(i % 3 === 0 && i % 5 === 0)) {
+      console.log(i);
     }
+  }
 }
-
-fizzBuzz[2]; 
-
+fizzBuzz(20);
 
 
 // De tarea, hacer el mismo ejercicio pero con un bulce while
