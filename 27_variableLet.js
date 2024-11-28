f81b4106f74b568680a07817d177689297f603dd
albecabrera <albe.cabrera@yahoo.de>
2024-11-28 15:50:57 +0100

    alcance de las variables

---
+++ 27_variableLet.js
@@ -0,0 +1,36 @@
+let age;
+age = 47;
+console.log(age);
+
+let nombre;
+nombre = "Alberto Cabrera";
+console.log(nombre);
+
+let oficio;
+oficio = "Profesor de Cultura Física y Deportes y de Español como lengua extranjera";
+console.log(oficio);
+
+let mobil = 'iPhone';
+console.log(mobil);
+
+// ALCANCE DE LAS VARIABLES 
+function foo() {
+  if (true) {
+    var x = 1o;
+    let y = 20;
+  }
+  console.log(x); // output: 10
+  console.log(y); // Uncaught ReferenceError: y is not defined
+}
+
+// OTRO EJEMPLO DEL ALCANCE DE LAS VARIABLES
+function foo() {
+  var x = 35;
+  if (true) {
+    let y = 20;
+    console.log(x); // output: 10
+    console.log(y); // output: 20
+  }
+  console.log(x); // output: 10
+  console.log(y); // Uncaught ReferenceError: y is not defined
+}
